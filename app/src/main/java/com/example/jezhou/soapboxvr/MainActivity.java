85f611bb5286199bcf011e7750ade62a77f06912
emilywang13 <emilyserenawang@gmail.com>
2015-10-03 21:42:25 -0700

    Added new activities + home page

--- app/src/main/java/com/example/jezhou/soapboxvr/MainActivity.java
+++ app/src/main/java/com/example/jezhou/soapboxvr/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +20,30 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button recordingButton = (Button) findViewById(R.id.recording_button);
+        recordingButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startRecordingActivity();
             }
         });
+
+        Button audienceButton = (Button) findViewById(R.id.audience_button);
+        audienceButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startAudienceActivity();
+            }
+        });
+
+        Button settingsButton = (Button) findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startSettingsActivity();
+            }
+        });
+
     }
 
     @Override
@@ -48,5 +66,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user touches the Recordings button */
+    public void startRecordingActivity() {
+        Intent intent = new Intent(this, Recordings.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user touches the Audience button */
+    public void startAudienceActivity() {
+        Intent intent = new Intent(this, Audience.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user touches the Settings button */
+    public void startSettingsActivity() {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 }
